2955a7614f29fe00ffe9b5beb792008e8c88c960
Matthew Morgan <mattmn@iastate.edu>
2025-02-17 13:01:48 -0600

    Got docker to run again

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,24 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  root: path.resolve(__dirname),
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
   server: {
     host: true,
     port: 5173,
     watch: {
       usePolling: true
+    }
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
     }
   }
 });
