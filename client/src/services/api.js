f8d589570879434df3000852186cd6807ff8300c
Matthew Morgan <mattmn@iastate.edu>
2025-02-17 13:39:00 -0600

    Change how base url is fetched

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
-console.log(process.env.REACT_APP_API_URL);
+
+// Debug log to see the value
+console.log('API URL:', import.meta.env.VITE_API_URL);
+
 export const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:8001/api',
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
   headers: {
     'Content-Type': 'application/json'
   }
